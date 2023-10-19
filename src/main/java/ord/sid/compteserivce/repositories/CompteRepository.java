38f336512f6ed2db056b30ff76be61a878399543
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-10-19 19:33:53 +0100

    RepositoryRestResource

--- src/main/java/ord/sid/compteserivce/repositories/CompteRepository.java
+++ src/main/java/ord/sid/compteserivce/repositories/CompteRepository.java
@@ -2,6 +2,8 @@ package ord.sid.compteserivce.repositories;
 
 import ord.sid.compteserivce.entities.Compte;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CompteRepository extends JpaRepository<Compte,Long> {
 }
